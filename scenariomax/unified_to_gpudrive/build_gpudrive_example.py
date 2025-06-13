1176718c1122a89489cad51eb13696b1c3ecf8ff
Ellington Kirby <ellingtonkirby@gmail.com>
2025-06-13 12:41:27 +0000

    [BUG] fix bad dict order

--- scenariomax/unified_to_gpudrive/build_gpudrive_example.py
+++ scenariomax/unified_to_gpudrive/build_gpudrive_example.py
@@ -402,8 +402,8 @@ def build_gpudrive_example(name, scenario_net_scene, debug=False):
             "map_name": metadata["map"],
             "objects_of_interest": [],
             "tracks_to_predict": [],
-            "scenario_info": metadata["scenario_type"] # for openscenes data this contains the scenario token
             "average_distance_traveled": _ensure_scalar(np.mean(objects_distance_traveled)),
+            "scenario_info": metadata["scenario_type"] # for openscenes data this contains the scenario token
         }
 
     scenario_dict = {
